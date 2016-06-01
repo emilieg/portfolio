259f4a6e1bf0e4a57a050d133250579dedcd1678
emilieg <emilie.gerber@hotmail.com>
2016-05-31 19:01:33 -0700

    debugging

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 $(function(){
-    $("#welcome").typed({
-        // strings: ["Welcome.", "My name is Emilie.", "I like building things for the web <3.", "Feel free to look around."],
-        stringsElement: $('#typed-strings'),
-        typeSpeed: 50,
-        contentType: 'html',
-    });
-//nav links to section
-// $('a').click(function(){
-//     $('html, body').animate({
-//         scrollTop: $( $(this).attr('href') ).offset().top
-//     }, 850);
-//     return false;
-// });
+    // $("#welcome").typed({
+    //     // strings: ["Welcome.", "My name is Emilie.", "I like building things for the web <3.", "Feel free to look around."],
+    //     stringsElement: $('#typed-strings'),
+    //     typeSpeed: 50,
+    //     contentType: 'html',
+    // });
+
+    // nav links to section
+    // $('a').click(function(){
+    //     $('html, body').animate({
+    //         scrollTop: $( $(this).attr('href') ).offset().top
+    //     }, 850);
+    //     return false;
+    // });
 
     $('#wrapper').fullpage({
         //Navigation
         menu: '#menu',
         lockAnchors: false,
-        anchors:['about', 'skills-section', 'projects', 'contact-section'],
+        anchors:['about-section', 'skills-section', 'projects-section', 'contact-section'],
         navigation: false,
         navigationPosition: 'right',
         // navigationTooltips: ['firstSlide', 'secondSlide'],
@@ -25,23 +26,25 @@ $(function(){
         slidesNavigation: true,
         slidesNavPosition: 'bottom',
         //added fixedElements for menu
-        // fixedElements: '#menu',
+        fixedElements: '#menu',
+
 
         //Scrolling
         css3: true,
         scrollingSpeed: 800,
-        autoScrolling: true,
+        autoScrolling: false,
         fitToSection: true,
         fitToSectionDelay: 1000,
-        scrollBar: false,
+        scrollBar: true,
         easing: 'easeInOutCubic',
         easingcss3: 'ease',
         loopBottom: false,
         loopTop: false,
         loopHorizontal: true,
         continuousVertical: false,
+        //
         normalScrollElements: '#element1, .element2',
-        scrollOverflow: false,
+        scrollOverflow: true,
         scrollOverflowOptions: null,
         touchSensitivity: 15,
         normalScrollElementTouchThreshold: 5,
@@ -50,10 +53,6 @@ $(function(){
         keyboardScrolling: true,
         animateAnchor: true,
         recordHistory: true,
-
-
-
-
 
 
 
