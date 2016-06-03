77560b45444b6d68c529b7566fea5d8206e3fd64
emilieg <emilie.gerber@hotmail.com>
2016-06-02 21:38:57 -0700

    added section slides

--- script.js
+++ script.js
@@ -1,74 +1,37 @@
 $(function(){
     // $("#welcome").typed({
-    //     // strings: ["Welcome.", "My name is Emilie.", "I like building things for the web <3.", "Feel free to look around."],
-    //     stringsElement: $('#typed-strings'),
-    //     typeSpeed: 50,
-    //     contentType: 'html',
+        // strings: ["Welcome.", "My name is Emilie.", "I like building things for the web <3.", "Feel free to look around."],
+        // stringsElement: $('#typed-strings'),
+        // typeSpeed: 100,
+        // contentType: 'html', 
+              // calback: function () {         
+              //   $('#wrapper').fullpage({
+              //       //Navigation
+              //       menu: '#menu',
+              //       anchors:['welcome-section','about-section', 'skills-section', 'projects-section', 'contact-section'],
+              //       navigation: false,
+              //       slidesNavigation: true,
+              //       css3: true,
+              //       scrollingSpeed: 1000,
+              //       responsiveWidth: 800,
+              //       responsiveHeight: 300
+              //   });
+              // } 
     // });
 
-    // nav links to section
-    // $('a').click(function(){
-    //     $('html, body').animate({
-    //         scrollTop: $( $(this).attr('href') ).offset().top
-    //     }, 850);
-    //     return false;
-    // });
 
     $('#wrapper').fullpage({
         //Navigation
         menu: '#menu',
-        lockAnchors: false,
-        anchors:['about-section', 'skills-section', 'projects-section', 'contact-section'],
+        anchors:['welcome-section','about-section', 'skills-section', 'projects-section', 'contact-section'],
         navigation: false,
-        navigationPosition: 'right',
-        // navigationTooltips: ['firstSlide', 'secondSlide'],
-        showActiveTooltip: false,
         slidesNavigation: true,
-        slidesNavPosition: 'bottom',
-        //added fixedElements for menu
-        fixedElements: '#menu',
-
-
-        //Scrolling
         css3: true,
-        scrollingSpeed: 800,
-        autoScrolling: false,
-        fitToSection: true,
-        fitToSectionDelay: 1000,
-        scrollBar: true,
-        easing: 'easeInOutCubic',
-        easingcss3: 'ease',
-        loopBottom: false,
-        loopTop: false,
-        loopHorizontal: true,
-        continuousVertical: false,
-        //
-        normalScrollElements: '#element1, .element2',
-        scrollOverflow: true,
-        scrollOverflowOptions: null,
-        touchSensitivity: 15,
-        normalScrollElementTouchThreshold: 5,
-
-        //Accessibility
-        keyboardScrolling: true,
-        animateAnchor: true,
-        recordHistory: true,
-
-
-
-
+        scrollingSpeed: 1000,
+        responsiveWidth: 800,
+        responsiveHeight: 300
     });
-
-
-
-
-
-
-
-
 });
-
-
 
 
 
