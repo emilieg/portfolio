960e09391dcc13d7dac1aa94338667caa5f55b2a
emilieg <emilie.gerber@hotmail.com>
2016-06-04 10:03:26 -0700

    portofolio screen shots

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
 $(function(){
-    // $("#welcome").typed({
-        // strings: ["Welcome.", "My name is Emilie.", "I like building things for the web <3.", "Feel free to look around."],
-        // stringsElement: $('#typed-strings'),
-        // typeSpeed: 100,
-        // contentType: 'html', 
-              // calback: function () {         
-              //   $('#wrapper').fullpage({
-              //       //Navigation
-              //       menu: '#menu',
-              //       anchors:['welcome-section','about-section', 'skills-section', 'projects-section', 'contact-section'],
-              //       navigation: false,
-              //       slidesNavigation: true,
-              //       css3: true,
-              //       scrollingSpeed: 1000,
-              //       responsiveWidth: 800,
-              //       responsiveHeight: 300
-              //   });
-              // } 
-    // });
+    $("#welcome-typer").typed({
+        strings: ["Welcome.", "My name is Emilie.", "I like building things for the web <3.", "Feel free to look around."],
+        stringsElement: $('#typed-strings'),
+        typeSpeed: 100,
+        contentType: 'html'
+    });
+});    
 
-
+$(function(){
     $('#wrapper').fullpage({
         //Navigation
         menu: '#menu',
@@ -30,9 +18,18 @@ $(function(){
         scrollingSpeed: 1000,
         responsiveWidth: 800,
         responsiveHeight: 300
+        // afterRender: function(){
+        //     var pluginContainer = $(this);
+        //     alert("The resulting DOM structure is ready");
+        //     $("#welcome").typed({
+        //         strings: ["Welcome.", "My name is Emilie.", "I like building things for the web <3.", "Feel free to look around."],
+        //         stringsElement: $('#typed-strings'),
+        //         typeSpeed: 100,
+        //         contentType: 'html'
+        //     });
+        // }
     });
 });
-
 
 
 
