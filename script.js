cfa600f1cefa32cc65c2fbd811adb536f4118af5
emilieg <emilie.gerber@hotmail.com>
2016-06-12 09:52:11 -0700

    color of arrows, title font, bottom nav

--- script.js
+++ script.js
@@ -18,16 +18,6 @@ $(function(){
         scrollingSpeed: 1000,
         responsiveWidth: 800,
         responsiveHeight: 300
-        // afterRender: function(){
-        //     var pluginContainer = $(this);
-        //     alert("The resulting DOM structure is ready");
-        //     $("#welcome").typed({
-        //         strings: ["Welcome.", "My name is Emilie.", "I like building things for the web <3.", "Feel free to look around."],
-        //         stringsElement: $('#typed-strings'),
-        //         typeSpeed: 100,
-        //         contentType: 'html'
-        //     });
-        // }
     });
 });
 
